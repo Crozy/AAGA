ae5047971293f732db488665760983e75435796b
issathink <issamht1@gmail.com>
2016-10-22 19:08:20 +0200

    Modified file

--- src/algorithms/DefaultTeam.java
+++ src/algorithms/DefaultTeam.java
@@ -1,10 +1,15 @@
 package algorithms;
 
 import java.awt.Point;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -16,35 +21,44 @@ import java.util.HashSet;
 
 public class DefaultTeam {
 	static HashMap<Point, ArrayList<Point>> voisins;
+	public static int i = 0;
+
 	public ArrayList<Point> calculConnectedDominatingSet(
 			ArrayList<Point> points, int edgeThreshold)
 			throws FileNotFoundException {
-		for(int i = 0 ; i< 100 ; i++){
-			File f = new File("input/input.points"+i);
-			FileOutputStream fo = new FileOutputStream(f);
-			try {
-				fo.write(10);
-			} catch (IOException e) {
-				e.printStackTrace();
+		
+		try {
+			File f = new File("input/input.points0");
+			PrintStream fr = new PrintStream(new FileOutputStream(f));
+			File f2 = new File("input.points");
+			BufferedReader fr2 = new BufferedReader(new FileReader(f2));
+			String ligne = "";
+			while ((ligne = fr2.readLine()) != null) {
+				fr.append(ligne+"\n");
 			}
-		}
-		
-		initVoisins(points, edgeThreshold);
-		ArrayList<Point> list = null;
-		ArrayList<Point> copypoints = pointsWithoutDoublon(points);
-		ArrayList<PointMIS> bestlist = null;
-		int bestsize = Integer.MAX_VALUE;
-		
-		for (int i = 0; i < copypoints.size(); i++) {
-				ArrayList<PointMIS> listMIS = SMIS.algoGlouton(copypoints,
-						edgeThreshold, i);
+			fr.close();
+			fr2.close();
 
-				if (bestsize > listMIS.size()) {
-					bestlist = listMIS;
-					bestsize = listMIS.size();
-				}
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-		
+
+		initVoisins(points, edgeThreshold);
+		ArrayList<Point> list = new ArrayList<Point>();
+		ArrayList<Point> copypoints = pointsWithoutDoublon(points);
+		ArrayList<PointMIS> bestlist = new ArrayList<PointMIS>();
+		int bestsize = Integer.MAX_VALUE;
+//
+//		for (int i = 0; i < copypoints.size(); i++) {
+//			ArrayList<PointMIS> listMIS = SMIS.algoGlouton(copypoints,
+//					edgeThreshold, i);
+//
+//			if (bestsize > listMIS.size()) {
+//				bestlist = listMIS;
+//				bestsize = listMIS.size();
+//			}
+//		}
+
 		list = SMIS.pointsFromPointsMIS(bestlist);
 		return list;
 	}
@@ -74,8 +88,8 @@ public class DefaultTeam {
 					copySteiner.add(p3);
 					if (Util.isValidAndConnexe(voisins, points, copySteiner,
 							edgeThreshold)) {
-							return copySteiner;
-					}else
+						return copySteiner;
+					} else
 						copySteiner.remove(p3);
 				}
 				copySteiner.add(p2);
